3c495f75f760f4747d5c7253388bfaa988bc8a66
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-11-16 15:02:34 +0100

    Fix pagination and add another serializer to MovieSession

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -113,8 +113,28 @@ class TicketSerializer(serializers.ModelSerializer):
         )
 
 
+class MovieSessionInTicketSerializer(serializers.ModelSerializer):
+    movie_title = serializers.CharField(source="movie.title", read_only=True)
+    cinema_hall_name = serializers.CharField(
+        source="cinema_hall.name", read_only=True
+    )
+    cinema_hall_capacity = serializers.IntegerField(
+        source="cinema_hall.capacity", read_only=True
+    )
+
+    class Meta:
+        model = MovieSession
+        fields = (
+            "id",
+            "movie_title",
+            "cinema_hall_name",
+            "cinema_hall_capacity",
+            "show_time",
+        )
+
+
 class TicketListSerializer(TicketSerializer):
-    movie_session = MovieSessionListSerializer(many=False, read_only=True)
+    movie_session = MovieSessionInTicketSerializer(many=False, read_only=True)
 
 
 class OrderSerializer(serializers.ModelSerializer):
